d621eb3e3a1048725b9184c577d64b259b780210
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-26 21:41:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.slice(0,array.size-1).join(", ") << " and #{array.last}"
+  else
+    array.slice(0,array.size-1).join(", ") << ", and #{array.last}"
+  end
 end
