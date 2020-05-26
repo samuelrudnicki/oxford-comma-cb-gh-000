55b0bb1f23c44fd7513a1042dccabf7a006cb836
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-26 21:39:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.size == 1
     array[0]
   else
-    array.slice(0,array.size-1).join(", ") << " and #{array.last}"
+    array.slice(0,array.size-1).join(", ") << ", and #{array.last}"
   end
 end
